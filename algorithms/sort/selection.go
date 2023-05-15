7f298b74236386bf1b671f341364b1aa885ec912
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2023-05-16 03:05:31 +0800

    add selection sort

---
+++ algorithms/sort/selection.go
@@ -0,0 +1,19 @@
+package sort
+
+func SelectionSort(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+
+	for i := 0; i < len(nums); i++ {
+		min := i
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < nums[min] {
+				min = j
+			}
+		}
+		if min != i {
+			nums[i], nums[min] = nums[min], nums[i]
+		}
+	}
+}
